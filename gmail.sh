4d487976531696ca17cdce10596822b3ea2264e4
BigbinaryHead <bigcodemonk@gmail.com>
2017-06-28 20:12:14 +0530

    Made password invisible when taking in shell using stty

--- gmail.sh
+++ gmail.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-username="akhilvantipalli"
-password="B@)T@\$T3m@N+^"
+echo -n "username: "
+read username
+echo -n "password: "
+# stty -echo to disable text
+stty -echo
+read password
+# stty echo to enable text
+stty echo
 echo
 curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom" |  grep -oPm1 "(?<=<title>)[^<]+" | sed '1d'
